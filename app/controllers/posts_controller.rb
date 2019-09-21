44b553221bf5e10d0fd79625e6810f5f1c742335
github eigi-fb <huuyaaa5@yahoo.co.jp>
2019-09-21 22:24:41 +0900

    post-show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,29 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    end
   end
 
   def index
   end
 
-  def create
-  end
+
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
   end
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
 end
+
