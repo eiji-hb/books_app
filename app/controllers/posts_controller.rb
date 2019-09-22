7639a42cecd2cf4a91f3e8b5e2307158ec6c6e3f
github eigi-fb <huuyaaa5@yahoo.co.jp>
2019-09-22 22:16:41 +0900

    f-refe

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,8 @@ class PostsController < ApplicationController
     @post = Post.new
   end
   def create
-    @post = Post.new(post_params)
+    @post = Post.new(post_params.merge(user_id: current_user.id))
+
     if @post.save
       redirect_to post_path(@post)
     end
@@ -16,21 +17,22 @@ class PostsController < ApplicationController
 
 
   def show
-    @post = Post.find(params[:id])
+    @post = current_user.posts.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
+    @post = current_user.posts.find(params[:id])
+
 
   end
 
   def update
-    @post = Post.find(params[:id])
+    @post = current_user.posts.find(params[:id])
     @post.update!(post_params)
     redirect_to post_path(@post)
   end
   def destroy
-    post = Post.find(params[:id])
+    @post = current_user.posts.find(params[:id])
     post.destroy
     redirect_to posts_path
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_21_100026) do
+ActiveRecord::Schema.define(version: 2019_09_22_111341) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "tests", force: :cascade do |t|
-    t.integer "user_id_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id_id"], name: "index_tests_on_user_id_id"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
