012b37f705e59afebd52b0d1a6d09d68e43c597f
github eigi-fb <huuyaaa5@yahoo.co.jp>
2019-09-22 18:07:27 +0900

    描画完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
   end
 
   def index
+    @posts = Post.all
   end
 
 
@@ -19,6 +20,19 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update!(post_params)
+    redirect_to post_path(@post)
+  end
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_path
   end
   private
 
