67ad603c4d3c18ece7066bd574d296d4898da0f2
github eigi-fb <huuyaaa5@yahoo.co.jp>
2019-09-23 15:47:33 +0900

    addImage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_22_111341) do
+ActiveRecord::Schema.define(version: 2019_09_23_050500) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_09_22_111341) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.text "image_data"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
